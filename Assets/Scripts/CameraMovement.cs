178ed8b4e04703e82cb4bfe13e8610d78e19fd0b
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-05-15 03:27:28 +0100

    Adding Player and Camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] float followSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 currentPos = transform.position;
+
+        if(followTarget != null)
+        {
+            Vector3 targetPos = followTarget.position;
+            Vector3 error = targetPos - currentPos;
+
+            targetPos = currentPos + error * followSpeed;
+
+            currentPos = new Vector3(targetPos.x, currentPos.y,currentPos.z);
+        }
+
+        transform.position = currentPos;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+
+    private float hAxis;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(hAxis * moveSpeed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+    }
+}
