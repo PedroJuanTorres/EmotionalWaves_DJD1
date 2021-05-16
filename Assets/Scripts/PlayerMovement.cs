2d8c572185bc04a935e30bdb33a3a102fbde64c0
PedroJuanTorres <74351644+PedroJuanTorres@users.noreply.github.com>
2021-05-16 12:18:57 +0100

    Adding animations to jump, run and idle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 10.0f;
+    [SerializeField]
+    private float jumpSpeed = 100.0f;
+    [SerializeField]
+    private float maxJumpTime = 0.1f;
+    [SerializeField]
+    private int jumpGravetyStart = 1;
+    [SerializeField]
+    private Transform groundCheckObject;
+    [SerializeField]
+    private float groundCheckRadius = 3.0f;
+    [SerializeField]
+    private LayerMask groundCheckLayer;
+
 
     private float hAxis;
-
     private Rigidbody2D rb;
+    private Animator animator;
+    private float timeOfJump;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +40,60 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //Moving on Axis
         hAxis = Input.GetAxis("Horizontal");
+
+        Collider2D collider = Physics2D.OverlapCircle(groundCheckObject.position, groundCheckRadius, groundCheckLayer);
+
+        bool isGround = (collider != null);
+
+        Vector2 currentVelocity = rb.velocity;
+
+        //Jumping mechanic
+        if(Input.GetButtonDown("Jump") && (isGround))
+        {
+            currentVelocity.y = jumpSpeed;
+
+            rb.velocity = currentVelocity;
+
+            rb.gravityScale = jumpGravetyStart;
+
+            timeOfJump = Time.time;
+        }
+        else
+        {
+            float elapsedTimeSinceJump = Time.time - timeOfJump;
+            if(Input.GetButton("Jump") && (elapsedTimeSinceJump < maxJumpTime))
+            {
+                rb.gravityScale = jumpGravetyStart;
+            }
+            else
+            {
+                rb.gravityScale = 5.0f;
+            }
+        }
+
+        if(currentVelocity.x < -0.1)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation.y = 180;
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+        else if(currentVelocity.x > 0.1)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation.y = 0;
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+
+        animator.SetFloat("AbsSpeedX", Mathf.Abs(currentVelocity.x));
+        animator.SetFloat("SpeedY", currentVelocity.y);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(groundCheckObject.position, groundCheckRadius);
+
     }
 }
