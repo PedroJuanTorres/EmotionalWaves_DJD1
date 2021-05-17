e0d8ebb6c7ee36b911fc484cc3e88dd897d029d2
PedroJuanTorres <74351644+PedroJuanTorres@users.noreply.github.com>
2021-05-17 14:27:31 +0100

    Starting to add the punch. Fixing animations, floor and walls coliders and backgrund

---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+
+    private CapsuleCollider2D punchCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        punchCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire3"))
+        { 
+           //punchCollider.enabled = True
+        }
+    }
+}
